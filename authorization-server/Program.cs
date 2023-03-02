49e7fbc35e43ffe172cc5396b8587d42f2651547
= <=>
2023-03-02 23:57:20 +0300

    json data parsing

--- authorization-server/Program.cs
+++ authorization-server/Program.cs
@@ -1,25 +1,29 @@
 using System.Data;
+using System.Text.Json;
 using authorization_server.DB;
 using authorization_server.Models;
+using Azure.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualBasic.CompilerServices;
 using Utils = authorization_server.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer(connection));
 
 var app = builder.Build();
 
-app.Map("/login/", () =>
+app.MapPost("/login/", (string loginData) =>
 {
-
+    var values = JsonSerializer.Deserialize<Dictionary<string, string>>(loginData);
+    return JsonSerializer.Serialize(Interaction.Login(values["login"], values["password"], values["refreshToken"]));
 });
 
-app.Map("/register/", () =>
+app.Map("/register/", (string registerData) =>
 {
-
+    var values = JsonSerializer.Deserialize<Dictionary<string, string>>(registerData);
+    return JsonSerializer.Serialize(Interaction.Register(values["login"], values["password"]));
 });
 
 app.MapGet("/", () =>
